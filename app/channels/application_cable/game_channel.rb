91fa4fb8b9a11b3e32b7141a47a8ff806487b17b
Rohan Mitchell <rohan@rohanmitchell.com>
2016-01-05 15:15:52 +1100

    Send and display initial world state

--- app/channels/application_cable/game_channel.rb
+++ app/channels/application_cable/game_channel.rb
@@ -1,9 +1,24 @@
+require 'agent_game/world'
+
 class GameChannel < ApplicationCable::Channel
   def subscribed
     stream_from "game_notifications"
   end
 
   def run_game
-    ActionCable.server.broadcast "game_notifications", {my: 'data'}
+    world = AgentGame::World.new 320, 200
+    broadcast_world_state world
   end
+
+
+  private
+
+  def broadcast_world_state world
+    broadcast type: 'world_state', world: world.data
+  end
+
+  def broadcast(data)
+    ActionCable.server.broadcast "game_notifications", data
+  end
+
 end
