6773044ccbcb1843b00a2655520e290da19741fc
Rohan Mitchell <rohan@rohanmitchell.com>
2016-01-12 15:36:39 +1100

    Create and place an agent in the game

--- lib/agent_land/game.rb
+++ lib/agent_land/game.rb
@@ -1,16 +1,32 @@
 require 'agent_land/world_generator'
+require 'agent_land/agent_presence'
 
 module AgentLand
   class Game
-    attr_reader :world
+    attr_reader :world, :agents
 
     def initialize
+      create_world
+      add_agents
+
+      @frame = 0
+    end
+
+    def create_world
       @world = World.new
       wg = WorldGenerator.new 320, 200
       wg.generate
       @world.data = wg.data
+    end
 
-      @frame = 0
+
+    def add_agents
+      @agents = []
+
+      ap = AgentPresence.new
+      @world.place ap
+
+      @agents << ap
     end
 
     def running?
--- spec/lib/agent_land/game_spec.rb
+++ spec/lib/agent_land/game_spec.rb
@@ -4,10 +4,19 @@ require 'agent_land/game'
 module AgentLand
   describe Game do
     let(:game) { Game.new }
+    let(:world) { game.world }
+    let(:agent) { game.agents.first }
 
     it "initialises a world" do
       expect(game.world).to be_a World
       expect(game.world.data).not_to be_empty
+    end
+
+    it "places the agent within the world bounds" do
+      expect(agent.x).to be >= 0
+      expect(agent.y).to be >= 0
+      expect(agent.x).to be < world.width
+      expect(agent.y).to be < world.height
     end
   end
 end
